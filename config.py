1e451ac1ee013af99d6541312ae97d3a8b5af5aa
sawe-daisy <deesawe16@gmail.com>
2020-11-02 15:05:50 +0300

    configurations

--- config.py
+++ config.py
@@ -3,17 +3,38 @@ import os
 class Config:
     # SECRET_KEY = os.environ.get('SECRET_KEY')
     QUOTE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:sawedaisy@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:sawedaisy@localhost/blogs'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SECRET_KEY ='~3og3YXm)#([yG@)I}La7(r1>fQ&7>'
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # DEBUG=True
+    # MAIL_USE_SSL =True
+    MAIL_USE_TLS = True
+    MAIL_MAX_EMAILS=None
+    MAIL_ASCII_ATTACHMENTS = False
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
-class DevConfig(Config):
-    DEBUG=True
 
 class ProdConfig(Config):
     pass
 
-config_options={
-    'development':DevConfig,
-    'production':ProdConfig
+class TestConfig(Config):
+    pass
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:sawedaisy@localhost/blogs'
+    DEBUG = True
+
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test': TestConfig
 }
