897084c83b47a38a87c72cee5979e175e600349c
sawe-daisy <deesawe16@gmail.com>
2020-11-02 08:13:24 +0300

    routes

---
+++ app/main/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import RadioField, TextAreaField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import Required, Email, EqualTo
+from wtforms import ValidationError
+
+class UpdateProfile(FlaskForm):
+    bio= TextAreaField('Tell us about yourself', validators=[Required()])
+    submit= SubmitField("submit here")
+
+class AddBlog(FlaskForm):
+    name = TextAreaField('Blog title', validators=[Required()]))
+    details = TextAreaField('Add your pitch', validators=[Required()]))
+    submit = SubmitField('Submit')
+
+class CommentsForm(FlaskForm):
+    details= TextAreaField("You can add your comment here", validators=[Required()])
+    submit=SubmitField('submit')
+
+class UpvoteForm(FlaskForm):
+    submit = SubmitField('submit')
+
+class DownvoteForm(FlaskForm):
+    submit=SubmitField('submit')
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,12 @@
-from flask import render_template
-from ..models import Quotes
+from flask import render_template,request,redirect,url_for,abort, flash, abort
 from . import main
+from flask_fontawesome import FontAwesome
+from .. import db, photos
+from flask_login import login_required, current_user
+from ..models import User, Pitch, Comment, Upvotes,Downvote, PhotoProfile
+from .forms import AddPitch, UpdateProfile, CommentsForm, UpvoteForm,DownvoteForm
+from flask.views import View, MethodView
+import markdown2
 from ..req import get_quotes
 
 @main.route('/')
