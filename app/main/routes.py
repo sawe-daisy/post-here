16e0925805ac2090369b4863c3e5c2b1a5cad655
sawe-daisy <deesawe16@gmail.com>
2020-11-02 09:26:26 +0300

    profile update

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,10 +1,9 @@
 from flask import render_template,request,redirect,url_for,abort, flash, abort
 from . import main
-from flask_fontawesome import FontAwesome
 from .. import db, photos
 from flask_login import login_required, current_user
-from ..models import User, Pitch, Comment, Upvotes,Downvote, PhotoProfile
-from .forms import AddPitch, UpdateProfile, CommentsForm, UpvoteForm,DownvoteForm
+from ..models import User, Blog, Comment, Upvotes,Downvote, PhotoProfile
+from .forms import AddBlog, UpdateProfile, CommentsForm, UpvoteForm,DownvoteForm
 from flask.views import View, MethodView
 import markdown2
 from ..req import get_quotes
@@ -14,3 +13,101 @@ def index():
     quotes= get_quotes()
 
     return render_template('index.html', quotes=quotes)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update', methods=['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username= uname).first()
+    if user is None:
+        abort(404)
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', uname=user.username))
+    return render_template('profile/update.html', form=form)
+
+@main.route('/user/<uname>/update/pic', methods=['POST'])
+@login_required
+def update_pic(uname):
+    user= User.query.filter_by(username=uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        user_photo = PhotoProfile(pic_path = path,user = user)
+        db.session.commit()
+    return redirect(url_for('main.profile', uname=uname))
+
+@main.route('/blogs/new/', methods= ['GET','POST'])
+@login_required
+def new_blog():
+    form = AddBlog()
+    upvotes = Upvotes.query.filter_by(blogs_id=Blog.id)
+    if form.validate_on_submit():
+        details= form.details.data
+        blog_id =current_user
+        title= form.name.data
+        # print(current_user._get_current_object().id)
+        new_blogs = Blog(blog_id=current_user._get_current_object().id, details=details, title=title)
+        new_blogs.save_pitches()
+
+        return redirect(url_for('main.index'))
+
+    return render_template('blog.html', form=form)
+
+@main.route('/comment/new/<int:blogs_id>', methods=['GET','POST'])
+# @login_required
+def add_comment(blogs_id):
+    form = CommentsForm()
+    blog = Blog.query.get(blogs_id)
+    if form.validate_on_submit():
+        details= form.details.data
+        add_comment= Comment(details=details, user_id=current_user._get_current_object().id, blogs_id=blogs_id)
+        db.session.add(add_comment)
+        db.session.commit()
+
+        return redirect(url_for('.add_comment', blogs_id=blogs_id))
+    allComents= Comment.query.filter_by(blogs_id=blogs_id).all()
+    return render_template('comment.html',form=form, allComents=allComents,blog =blog)
+
+@main.route('/blog/upvote/<int:blogs_id>/upvote', methods=['GET', 'POST'])
+# @login_required
+def upvote(blogs_id):
+    blog = Blog.query.get(blogs_id)
+    user = current_user
+    p_upvotes =Upvotes.query.filter_by(blogs_id=blogs_id)
+
+    if Upvotes.query.filter(Upvotes.user_id==user.id, Upvotes.blogs_id==blogs_id).first():
+        return redirect(url_for('main.index'))
+
+    newUpvote = Upvotes(blogs_id=blogs_id, user=current_user)
+    newUpvote.save_upvotes()
+    return redirect(url_for('main.index'))
+
+
+
+@main.route('/blog/downvote/<int:blogs_id>/downvote', methods=['GET','POST'])
+# @login_required
+def downvote(blogs_id):
+    blog= Blog.query.get(blogs_id)
+    user = current_user
+    douwnVotes= Downvote.query.filter_by(blogs_id=blogs_id)
+
+    if Downvote.query.filter(Downvote.user_id==user.id, Downvote.blogs_id==blogs_id).first():
+        return redirect(url_for('main.index'))
+
+    newDownvote = Downvote(blogs_id=blogs_id, user=current_user)
+    newDownvote.save_downvotes()
+    return redirect(url_for('main.index'))
--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
+from . import db
+from sqlalchemy.sql import func
+from flask_login import UserMixin, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 class Quotes:
      
      quote_list=[]
@@ -7,4 +17,133 @@ class Quotes:
          self.author = author
          self.quote=quote
          self.permalink=permalink
-         
+
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key= True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    pass_word= db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    photos = db.relationship('PhotoProfile',backref = 'user',lazy = "dynamic")
+    blog = db.relationship('Blog', backref='user', lazy='dynamic')
+    comments = db.relationship('Comment', backref = 'user', lazy = 'dynamic')
+    upvotes = db.relationship('Upvotes', backref = 'user', lazy = 'dynamic')
+    downvotes = db.relationship('Downvote', backref = 'user', lazy = 'dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_word = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_word,password)
+
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class PhotoProfile(db.Model):
+    __tablename__ = 'profile_photos'
+
+    id = db.Column(db.Integer,primary_key = True)
+    pic_path = db.Column(db.String())
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+class Blog(db.Model):
+    __tablename__='blogs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    blog_id= db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    details= db.Column(db.String(), index=True)
+    title = db.Column(db.String(255), nullable=False)
+    comments = db.relationship('Comment',backref='blog',lazy='dynamic')
+    upvotes = db.relationship('Upvotes', backref = 'blog', lazy = 'dynamic')
+    downvotes = db.relationship('Downvote', backref = 'blog', lazy = 'dynamic')
+
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    @classmethod
+    def getblog(cls, id):
+        blogs = Blog.query.order_by(blogId=id).desc().all()
+        return blogs
+
+    def __repr__(self):
+        return f'Blog {self.details}'
+
+class Comment(db.Model):
+    __tablename__='comments'
+    id =db.Column(db.Integer, primary_key=True)
+    blogs_id = db.Column(db.Integer, db.ForeignKey('blogs.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable= False)
+    details = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'Comment: id:{self.id} comment: {self.details}'
+
+class Upvotes(db.Model):
+    __tablename__='upvotes'
+    id = db.Column(db.Integer, primary_key=True)
+    upvote = db.Column(db.Integer, default=0)
+    blogs_id = db.Column(db.Integer,db.ForeignKey('blogs.id'))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+    def save_upvotes(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+    def add_upvotes(cls,id):
+        upvote_pitch = Upvotes(user = current_user, blogs_id=id)
+        upvote_pitch.save_upvotes()
+
+    
+    @classmethod
+    def get_upvotes(cls,id):
+        upvote = Upvotes.query.filter_by(blogs_idid).all()
+        return upvote
+
+    @classmethod
+    def get_all_upvotes(cls,blogs_id):
+        upvotes = Upvotes.query.order_by('id').all()
+        return upvotes
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitches_id}'
+
+class Downvote(db.Model):
+    __tablename__= 'downvotes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    downvote =db.Column(db.Integer, default= 0)
+    blogs_id = db.Column(db.Integer,db.ForeignKey('blogs.id'))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+    def save_downvotes(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def add_downvotes(cls,id):
+        downvote_blog = Downvote(user = current_user, blogs_id=id)
+        downvote_blog.save_downvotes()
+
+    
+    @classmethod
+    def get_downvotes(cls,id):
+        downvote = Downvote.query.filter_by(blogs_id=id).all()
+        return downvote
+
+    @classmethod
+    def get_all_downvotes(cls,blogs_id:
+        downvote = Downvote.query.order_by('id').all()
+        return downvote
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.blogs_id}'
